2b072652cc1e0fd206446a40f1b86d34ab8ef4b2
hh2003122383 <hh2003122383@gmail.com>
2026-03-09 15:51:20 +0800

    completion

--- Car-Counter.py
+++ Car-Counter.py
@@ -2,6 +2,7 @@ from ultralytics import YOLO
 import cv2
 import cvzone
 import math
+from sort import *
 
 cap = cv2.VideoCapture("data/cars.mp4")  
 
@@ -19,44 +20,75 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
               "teddy bear", "hair drier", "toothbrush"
               ]
+mask = cv2.imread("data/mask.png")
+
+# Tracking
+# max_age: Grace period for lost objects
+# min_hits: Activation threshold
+# iou_threshold: Matching strictness
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
+limits = [400,297,673,297]
+totalCount=[]
+
+# Ensure "cars.mp4" and "mask.png" consistent dimensions
+success, img = cap.read()
+if success:
+    mask = cv2.resize(mask, (img.shape[1], img.shape[0]))
 
 while True:
     success, img = cap.read()
-    # 4. 進行影像推理，使用 stream=True 提高處理效率
-    if not success:
-        print("影片讀取失敗或已播放結束。")
-        break
+    imgRegion = cv2.bitwise_and(img, mask)
 
-    results = model(img, stream=True)
+    imgGraphics = cv2.imread("data/graphics.png",cv2.IMREAD_UNCHANGED)
+    img = cvzone.overlayPNG(img, imgGraphics,(0,0))
 
-    # 5. 尋訪結果並提取邊界框、信心度與類別
+    results = model(imgRegion, stream=True)
+
+    detections = np.empty((0, 5))
+
     for r in results:
         boxes = r.boxes
         for box in boxes:
-            
-            # 獲取邊界框座標 X1, Y1, X2, Y2
+            # Bounding Box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            
-            # 計算寬度 (w) 與高度 (h) 給 cvzone 使用
             w, h = x2 - x1, y2 - y1
             
-            # 繪製角落風格的矩形邊界框
-            cvzone.cornerRect(img, (x1, y1, w, h), l=15)
-            
-            # 計算信心度 (四捨五入到小數點後兩位)
             conf = math.ceil((box.conf[0] * 100)) / 100
-            
-            # 取得預測類別 ID，並轉換為類別名稱
+            # Class Name
             cls = int(box.cls[0])
             currentClass = classNames[cls]
-            
-            # 在邊界框上方顯示類別與信心度，設定 max 防止文字超出視窗邊緣
-            cvzone.putTextRect(img, f'{currentClass} {conf}', 
-                               (max(0, x1), max(35, y1)), 
-                               scale=0.6, thickness=1, offset=3)
 
-cv2.imshow("Image", img)
-cv2.waitKey(1)
+            if currentClass in ["car", "truck", "bus", "motorbike"] and conf > 0.3:
+                # cvzone.putTextRect(img, f'{currentClass} {conf}', (max(0, x1), max(35, y1)), 
+                #                 scale=0.6, thickness=1, offset=3)
+                # cvzone.cornerRect(img, (x1, y1, w, h), l=7, rt=5)
+                currentArray = np.array([x1,y1,x2,y2,conf])
+                detections = np.vstack((detections,currentArray))
 
+    resultTracker = tracker.update(detections)
+    cv2.line(img, (limits[0],limits[1]),  (limits[2],limits[3]), (0,0,255),5)
     
+    for results in resultTracker:
+        x1,y1,x2,y2,id = results
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        print(results)
+        w, h = x2 - x1, y2 - y1
+        cvzone.cornerRect(img, (x1, y1, w, h), l=7, rt=2, colorR=(255,0,255))
+        cvzone.putTextRect(img, f'{(id)}', (max(0, x1), max(35, y1)), 
+                                scale=2, thickness=3, offset=10)
+        cx, cy = x1+w//2 , y1+h//2
+        cv2.circle(img, (cx,cy), 5 ,(255,0,255),cv2.FILLED)
+
+        if limits[0] < cx < limits[2] and limits[1]-15 < cy < limits[2]+15:
+            if totalCount.count(id) == 0:
+                totalCount.append(id)
+                cv2.line(img, (limits[0],limits[1]),  (limits[2],limits[3]), (0,0,255),5)
+
+    # cvzone.putTextRect(img, f' Count:{len(totalCount)}', (50,50))
+    cv2.putText(img, str(len(totalCount)), (255,100), cv2.FONT_HERSHEY_PLAIN,5,(50,50,255),8)
+        
+    cv2.imshow("Image", img)
+    # cv2.imshow("ImageRegion", imgRegion)
+    cv2.waitKey(1)
